49043bc27c2d998128b24a33fe85581de8978676
Juhoru4 <jhoyosr1@correo.usbcali.edu.co>
2026-05-18 20:47:14 -0500

    base logica del runner

---
+++ src/js/runner/player.js
@@ -0,0 +1,133 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124/build/three.module.js';
+
+import {MTLLoader} from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/loaders/MTLLoader.js';
+import {OBJLoader} from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/loaders/OBJLoader.js';
+
+
+export const player = (() => {
+
+  class Player {
+    constructor(params) {
+      this.position_ = new THREE.Vector3(0, 0, 0);
+      this.velocity_ = 0.0;
+
+      // this.mesh_ = new THREE.Mesh(
+      //     new THREE.BoxBufferGeometry(1, 1, 1),
+      //     new THREE.MeshStandardMaterial({
+      //         color: 0x80FF80,
+      //     }),
+      // );
+      // this.mesh_.castShadow = true;
+      // this.mesh_.receiveShadow = true;
+      // params.scene.add(this.mesh_);
+
+      this.playerBox_ = new THREE.Box3();
+
+      this.params_ = params;
+
+      this.LoadModel_();
+      this.InitInput_();
+    }
+
+    LoadModel_() {
+      const mtlLoader = new MTLLoader();
+      mtlLoader.setPath('../src/models/characters/');
+      mtlLoader.load('Velociraptor.mtl', (materials) => {
+        materials.preload();
+
+        const loader = new OBJLoader();
+        loader.setMaterials(materials);
+        loader.setPath('../src/models/characters/');
+        loader.load('Velociraptor.obj', (obj) => {
+          obj.scale.setScalar(0.3);
+          obj.quaternion.setFromAxisAngle(
+              new THREE.Vector3(0, 1, 0), Math.PI / 2);
+          obj.position.y = 0.1;
+
+          this.mesh_ = obj;
+          this.params_.scene.add(this.mesh_);
+
+          obj.traverse(c => {
+            let materials = c.material;
+            if (!(c.material instanceof Array)) {
+              materials = [c.material];
+            }
+
+            for (let m of materials) {
+              if (m) {
+                m.specular = new THREE.Color(0x000000);
+                m.color.offsetHSL(0, 0, 0.25);
+              }
+            }
+            c.castShadow = true;
+            c.receiveShadow = true;
+          });
+        });
+      });
+    }
+
+    InitInput_() {
+      this.keys_ = {
+          spacebar: false,
+      };
+      this.oldKeys = {...this.keys_};
+
+      document.addEventListener('keydown', (e) => this.OnKeyDown_(e), false);
+      document.addEventListener('keyup', (e) => this.OnKeyUp_(e), false);
+    }
+
+    OnKeyDown_(event) {
+      switch(event.keyCode) {
+        case 32:
+          this.keys_.space = true;
+          break;
+      }
+    }
+
+    OnKeyUp_(event) {
+      switch(event.keyCode) {
+        case 32:
+          this.keys_.space = false;
+          break;
+      }
+    }
+
+    CheckCollisions_() {
+      const colliders = this.params_.world.GetColliders();
+
+      this.playerBox_.setFromObject(this.mesh_);
+
+      for (let c of colliders) {
+        const cur = c.collider;
+
+        if (cur.intersectsBox(this.playerBox_)) {
+          this.gameOver = true;
+        }
+      }
+    }
+
+    Update(timeElapsed) {
+      if (this.keys_.space && this.position_.y == 0.0) {
+        this.velocity_ = 30;
+      }
+
+      const acceleration = -75 * timeElapsed;
+
+      this.position_.y += timeElapsed * (
+          this.velocity_ + acceleration * 0.5);
+      this.position_.y = Math.max(this.position_.y, 0.0);
+
+      this.velocity_ += acceleration;
+      this.velocity_ = Math.max(this.velocity_, -100);
+
+      if (this.mesh_) {
+        this.mesh_.position.copy(this.position_);
+        this.CheckCollisions_();
+      }
+    }
+  };
+
+  return {
+      Player: Player,
+  };
+})();
